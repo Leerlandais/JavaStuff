6de6e47920c16d87717e455de7b69e655bd46e75
Leerlandais <leerlandais@gmail.com>
2023-11-25 19:10:46 +0100

    added start button. Really stopping now

--- scripts/simon.js
+++ scripts/simon.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    document.getElementById("butStart").onclick = startSimon;
+
+    function startSimon(){
+        document.getElementById("butStart").style.opacity = "0";
+        alert("x");
+    }
 
 });
