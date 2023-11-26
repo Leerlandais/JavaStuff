ae815af96efd9461353399ef66acee4f1fc7cc89
Leerlandais <leerlandais@gmail.com>
2023-11-26 17:12:49 +0100

    Pieces can now move...sort of

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,6 +1,6 @@
 // document.addEventListener("DOMContentLoaded", function(){           Doesn't work with this turned on
 
-let previousElement = null;
+var previousElement = null;
     function selectPiece(element, color) {
         if (previousElement !== null) {
             previousElement.style.color = ''; 
@@ -9,6 +9,24 @@ let previousElement = null;
         previousElement = element;
     }
 
+var firstClick = null;
+    document.getElementById("chessboard").addEventListener("click", function(event) {
+        if (event.target.classList.contains("square")) {
+            console.log('Clicked square:', event.target.id);
+            var pickSquare = event.target;
+            
 
+            if (firstClick === null){
+            firstClick = pickSquare;
+            }
+            else {
+            var movePiece = firstClick.textContent;
+            pickSquare.textContent = movePiece;
+            firstClick.textContent = "";
+            firstClick = null;
+            }
+        }
+    
+    });
 
 // });
