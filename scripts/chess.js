9a3527d60b7397b0cd030b1a607c084e4c212056
Leerlandais <leerlandais@gmail.com>
2023-11-26 13:17:35 +0100

    chess lights up

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,6 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
+// document.addEventListener("DOMContentLoaded", function(){           Doesn't work with this turned on
+
+let previousElement = null;
+    function selectPiece(element, color) {
+        if (previousElement !== null) {
+            previousElement.style.color = ''; 
+        }
+        element.style.color = color;
+        previousElement = element;
+    }
 
 
 
-
-});
+// });
