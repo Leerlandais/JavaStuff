4e82de02c885179b1115a5fb331d2c1d8cd0e833
Leerlandais <leerlandais@gmail.com>
2023-11-28 09:20:57 +0100

    next step, player input

--- JavaStuff/scripts/newSimon.js
+++ JavaStuff/scripts/newSimon.js
@@ -6,31 +6,44 @@ var myGoCounter = 0;                                                            
     function GameStart() {                                                                                                              // starts the game
         var myGoCounter = 0;                                                                                                            // sets myGoCounter to 0. I'll change this later to be incremental
         var myPicks = [];                                                                                                               // creates an array to store my random choices
+        var lastPick = 0;
     
         function RunChoice() {                                                                                                          // starts the function, nested in GameStart
             if (myGoCounter < 5) {                                                                                                      // checks the repeat counter
                 document.getElementById("redBut").style.backgroundColor = "antiquewhite";                                               // changes colour back to default
                 document.getElementById("greBut").style.backgroundColor = "antiquewhite";            
                 document.getElementById("yelBut").style.backgroundColor = "antiquewhite";            
-                document.getElementById("bluBut").style.backgroundColor = "antiquewhite";              
+                document.getElementById("bluBut").style.backgroundColor = "antiquewhite";    
+                console.log("goes", myGoCounter +1);          
 
                 var myPick = Math.floor(Math.random() * 4) + 1;                                                                         // Picks a number
-                if (myPick === 1) {                                                                                                     // Decides action based on random
+                if (myPick === lastPick){                                                                                               // prevents number repetition (temporary until I find out how to make them flash better)
+                    console.log("same pick");
+                    myGoCounter--;                                                                                                      // reduces go counter to ensure 5 numbers are picked
+                    RunChoice();
+                }
+                
+                else if (myPick === 1) {                                                                                                // Decides action based on random
+                    console.log("1");
+                    lastPick = myPick;
                     myPicks.push(myPick);                                                                                               // add the chosen number to Array
                     pingRed();                                                                                                          // calls the relevant function
 
                 } else if (myPick === 2) {
                     console.log("2");
+                    lastPick = myPick;
                     myPicks.push(myPick);
                     pingGre();
 
                 } else if (myPick === 3) {
                     console.log("3");
+                    lastPick = myPick;
                     myPicks.push(myPick);
                     pingYel();
 
                 } else {
                     console.log("4");
+                    lastPick = myPick;
                     myPicks.push(myPick);
                     pingBlu();
 
@@ -39,10 +52,12 @@ var myGoCounter = 0;                                                            
     
                 if (myGoCounter >= 5) {                                                                                                 // checks if repeats are done...
                     console.log("doneRunChoice");                                                                                       // ...and logs this
+                    lastPick = 0;
+                    document.getElementById("PlayerTurn").textContent = "Press the buttons in the same order";                          // signals start of player turn                                                                                                 
                 
                 } else {   
 
-                    setTimeout(RunChoice, 1000);                                                                                        // Delay execution by 1 second for each loop
+                    setTimeout(RunChoice, 750);                                                                                         // Delay execution by 1 second for each loop
                 }
             }
 
@@ -50,26 +65,30 @@ var myGoCounter = 0;                                                            
                 document.getElementById("redBut").style.backgroundColor = "red";                                                        // changes colour of button
                 console.log("MyArray : " + myPicks);                                                                                    // outputs new Array to console
                 console.log("1");                                                                                                       // outputs to console
+                //    setTimeout(pingRed, 500);                                                                                                      
             }
             function pingGre () {
                 document.getElementById("greBut").style.backgroundColor = "green";
                 console.log("MyArray : " + myPicks);                                                                                    
-                console.log("2");                                                                                                       
+                console.log("2");
+                //    setTimeout(pingGre, 500);                                                                                                       
             }
             function pingYel () {
                 document.getElementById("yelBut").style.backgroundColor = "yellow";
                 console.log("MyArray : " + myPicks);                                                                                    
-                console.log("3");                                                                                                       
+                console.log("3");
+                //    setTimeout(pingYel, 500);                                                                                                       
             }
             function pingBlu () {
                 document.getElementById("bluBut").style.backgroundColor = "blue";
                 console.log("MyArray : " + myPicks);                                                                                    
-                console.log("4");                                                                                                       
+                console.log("4");
+                //    setTimeout(pingBlu, 500);                                                                                                       
             }
         }
     
-        RunChoice();                                                                                                                    // Restarts everything (ubtil counter is hit)
+        RunChoice();                                                                                                                    // Restarts everything (until counter is hit)
     }
     
-        // FOR TOMORROW: Place buttons, get theem to colour when pinged
+
 });
