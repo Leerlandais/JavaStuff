07967f38a9e302840443dda8da8b22310d72bef5
Leerlandais <leerlandais@gmail.com>
2023-12-07 17:59:37 +0100

    newer mines, might need surrounding grid

--- scripts/newermine.js
+++ scripts/newermine.js
@@ -4,6 +4,8 @@ var diff = "";
 var bombCount = 0;
 var hasBomb = new Set();
 var bombPos = [];
+var bombHint = new Array(100).fill(0); 
+let bombHintArray = [];
 
 document.getElementById("diff1").onclick = gameEasy;
 document.getElementById("diff2").onclick = gameMid;
@@ -12,17 +14,20 @@ document.getElementById("diff3").onclick = gameHard;
 
 function gameEasy() {
     console.log("Easy");
-    diff = 64;
+    diff = 100;
+    bombCount = 10;
     makeMine();
 }
 function gameMid() {
     console.log("Mid");
     diff = 100;
+    bombCount = 16;
     makeMine();
 }
 function gameHard() {
     console.log("Hard");
-    diff = 144;
+    diff = 100;
+    bombCount = 22;
     makeMine();
 }
 
@@ -39,28 +44,59 @@ function makeMine() {
         const placeGrid = document.createElement("div");
           placeGrid.classList.add("mineSquare");
           placeGrid.id = (i + 1); 
-          placeGrid.textContent = i + 1;
+    //      placeGrid.textContent = i + 1;
           gridMaker.appendChild(placeGrid);
     }
-    bombCount = Math.sqrt(parseInt(diff));
+   // bombCount = Math.sqrt(parseInt(diff));
     while (hasBomb.size < bombCount){
       const bombHere = Math.floor(Math.random() * diff);
       if (!hasBomb.has(bombHere)){
       hasBomb.add(bombHere);
     }
    }
+    bombHintArray = Array.from(hasBomb);
+    console.log("arry :", bombHintArray);
     bombPos = Array.from(hasBomb);
     console.log(hasBomb, bombCount);
    for (i = 0; i < bombCount; i++){
         document.getElementById(bombPos[i]).style.backgroundColor = "red";
    }
-}
-/* adjust this so that it creates an 8*8 grid, 10*10 or 12*12
+   console.log("size", bombHintArray.length);
+   
+   for (var i = 0; i < bombHintArray.length; i++){                 // start a loop that runs for the length of hasBomb (which is 8 but can be increased easily to any size)  
+          console.log("hasBomb", bombHintArray);                //   again an example of using log to find problems. I put this here because the rest of the loop wasn't functioning correctly and I wanted to ensure hasBomb was still complete (which it was, meaning my problem is further along) - Update, it's an hour later and I've fixed it
+          var bombHintAdd = bombHintArray[i];                                                                  // sets any position containing a bomb to a value far higher than any other square (this will be used later to verify if a bomb is clicked )
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +9;            // I really want to find a way to do this cleaner. It searches any square adjacent to a bomb and increases its value by 1 - there has to be a way to reduce the load
+          bombHintAdd = bombHintArray[i] -11;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] -10;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] -9;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] -1;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] +1;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] +9;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] +10;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+          bombHintAdd = bombHintArray[i] +11;
+          bombHint[bombHintAdd] = bombHint[bombHintAdd] +1;
+        }
+        console.log("bombHint : ", bombHint, bombHintArray);
+   
+    for (i = 1; i < (bombHint.length); i++){
+        if (document.getElementById(i) != null){
+            document.getElementById(i).textContent = bombHint[i];
+        }else {
+            continue;
+        }
+    }   
+        
+    }
 
-
-
-
-
+/*
     gridArray.push(placeGrid.id);  
     placeGrid.textContent = placeGrid.id;
  }
