36d11ff8c8f7788399f270e49fcfbc6f2451d38b
Leerlandais <leerlandais@gmail.com>
2023-12-16 11:47:34 +0100

    making a mess

--- scripts/newdungeon.js
+++ scripts/newdungeon.js
@@ -7,36 +7,57 @@ const playerArmour = document.getElementById("playerArmour");
 const playerSpeed = document.getElementById("playerSpeed");
 const playerLuck = document.getElementById("playerLuck");
 const dungInfo = document.getElementById("dungInfo");
-var playHP = 0;
-var playArm = 0;
-var playSpd = 0;
-var playLck = 0;
+const enemyClass = document.getElementById("enemyClass");
+const enemyHP = document.getElementById("enemyHP");
+const enemyArmour = document.getElementById("enemyArmour");
+const enemySpeed = document.getElementById("enemySpeed");
+const enemyLuck = document.getElementById("enemyLuck");
+var diceRoll1 = "";
+var diceRoll2 = "";
+
+
+var playerCard = {
+        playClass : "",
+        playHP : 0,
+        playArm : 0,
+        playSpd : 0,
+        playLck : 0
+      };
+      var nmeCard = {
+        nmeClass : "Wolf",
+        nmeHP : 100,
+        nmeArm : 25,
+        nmeSpd : 10,
+        nmeLck : 10
+      };
+
 spinDice.style.display = "none";
 
-
+startDung.style.color = "green";
 startDung.onclick = makeChar;
 
 function makeChar() {
-
+  startDung.style.color = "black";
   startDung.textContent = "Continue";
   dungHeader.textContent = "Let's pick a Class";
   playerClass.textContent = "For the moment there isn't any, so go ahead and click Continue";
-  playerHP.textContent = "HP : ";
-  playerArmour.textContent = "Armour : ";
-  playerSpeed.textContent = "Speed : ";
-  playerLuck.textContent = "Luck : ";
+  playerCard.playClass = "Warrior";
+  playerHP.textContent = "HP : " + playerCard.playHP;
+  playerArmour.textContent = "Armour : " + playerCard.playArm;
+  playerSpeed.textContent = "Speed : " + playerCard.playSpd;
+  playerLuck.textContent = "Luck : " + playerCard.playLck;
   dungInfo.textContent = "Click below to roll your stats";
   startDung.onclick = rollStats;
 }
 
 function rollStats () {
-  startDung.style.display = "none";
+  startDung.disabled = true;
   dungHeader.textContent = "Roll for HP";
-  playerClass.textContent = "As I said before but let's say you picked Warrior";
-  playerHP.textContent = "HP : ";
-  playerArmour.textContent = "Armour : ";
-  playerSpeed.textContent = "Speed : ";
-  playerLuck.textContent = "Luck : ";
+  playerClass.textContent = playerCard.playClass;
+  playerHP.textContent = "HP : " + playerCard.playHP;
+  playerArmour.textContent = "Armour : " + playerCard.playArm;
+  playerSpeed.textContent = "Speed : " + playerCard.playSpd;
+  playerLuck.textContent = "Luck : " + playerCard.playLck;
   dungInfo.textContent = ""; 
   spinDice.style.display = "";
   spinDice.onclick = rollHP;
@@ -44,34 +65,69 @@ function rollStats () {
 
 function rollHP() {
   dungHeader.textContent ="Now for Armour";
-  playerClass.textContent = "Warrior";
-  playHP = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
-  playerHP.textContent = "HP : " + playHP;
+  playerCard.playHP = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
+  playerHP.textContent = "HP : " + playerCard.playHP;
   spinDice.onclick = rollArmour;
 }
 
 function rollArmour() {
   dungHeader.textContent ="Next for Speed";
-  playArm = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
-  playerArmour.textContent = "Armour : " + playArm; 
+  playerCard.playArm = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
+  playerArmour.textContent = "Armour : " + playerCard.playArm; 
   spinDice.onclick = rollSpeed;
 }
 
 function rollSpeed() {
   dungHeader.textContent ="Once more for Luck";
-  playSpd = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
-  playerSpeed.textContent = "Speed : " + playSpd;
+  playerCard.playSpd = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
+  playerSpeed.textContent = "Speed : " + playerCard.playSpd;
   spinDice.onclick = rollLuck;
 }
 
 function rollLuck() {
-  dungHeader.textContent ="That's all for now and not bad for an evening's work";
-  playLck = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
-  playerLuck.textContent = "Luck : " + playLck;
-  //spinDice.onclick = rollLuck;
+  dungHeader.textContent ="That's your char rolled";
+  playerCard.playLck = Math.floor((rnd + rnd2)*8 +(rnd + rnd2)/3);
+  playerLuck.textContent = "Luck : " + playerCard.playLck;
+  startDung.disabled = false;
+  spinDice.disabled = true;
+  startDung.textContent = "Step Outside";
+  startDung.onclick = advanceStep;
+}
+
+function advanceStep() {
+  startDung.textContent = "Continue";
+  dungHeader.textContent = "Fight";
+  enemyClass.textContent = nmeCard.nmeClass;
+  enemyHP.textContent = "HP : " + nmeCard.nmeHP;
+  enemyArmour.textContent = "Armour : " + nmeCard.nmeArm;
+  enemySpeed.textContent = "Speed : " + nmeCard.nmeSpd;
+  enemyLuck.textContent = "Luck : " + nmeCard.nmeLck;
+  diceRoll1 = "";
+  diceRoll2 = "";
+  startDung.onclick = fightEvent;
 }
 
 
+
+
+    /*
+    if (playerCard.playHP < 1) {
+      dungInfo.textContent = "You died";
+      spinDice.disabled = true;
+      startDung.disabled = false;
+      startDung.textContent = "Click to Restart";
+      startDung.onclick = reloadPage;
+    }else if (nmeCard.nmeHP < 1) {
+      dungInfo.textContent = "You win";
+      spinDice.disabled = true;
+      startDung.disabled = false;
+      startDung.textContent = "Click to continue";
+      startDung.onclick = reloadPage;
+    }
+    */
+function reloadPage() {
+  location.reload();
+}
 
 
 
@@ -143,7 +199,7 @@ $("#dice-color").val("#000000");
             y = 900;
             break;
         }
-        console.log("1 : ", rnd, x, y);
+  //      console.log("1 : ", rnd, x, y);
         if(rnd === lastRnd) {
             console.log ("Do a Roll");
             x += 360;
@@ -165,23 +221,27 @@ $("#dice-color").val("#000000");
             break;
         }
         if(rnd2 === lastRnd2) {
-            console.log ("Do a Roll");
+  //          console.log ("Do a Roll");
             w += 360;
             z += 360;
         }
-        console.log("2 : ", rnd2, w, z);
+  //      console.log("2 : ", rnd2, w, z);
 
  $(".dice").css(
  "transform",
  "translateZ(-100px) rotateY(" + x + "deg) rotateX(" + y + "deg)"
   );
+  diceRoll1 = ""
   lastRnd = rnd;
+  diceRoll1 = rnd;
   $(".dice.dice2").css(
     "transform",
     "translateZ(-100px) rotateY(" + w + "deg) rotateX(" + z + "deg)"
      );
+     diceRoll2 = "";
     lastRnd2 = rnd2;
+    diceRoll2 = rnd2;
 setInterval(() => {
     document.getElementById("spinDice").style.display = "initial";
-}, 3000);  
+}, 1000);  
 });
